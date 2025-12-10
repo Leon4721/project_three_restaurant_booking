6d6e121d998e80792c49cee61a3b22af94ed3a3d
Leon4721 <leonfreeman6@gmail.com>
2025-12-10 02:44:50 +0000

    Auto-create default superuser for Render

--- booking_app/apps.py
+++ booking_app/apps.py
@@ -2,4 +2,27 @@ from django.apps import AppConfig
 
 
 class BookingAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'booking_app'
+
+    def ready(self):
+        """
+        Auto-create a default superuser on first run in environments
+        where we can't access manage.py createsuperuser (like Render free tier).
+        """
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError, ProgrammingError
+
+        User = get_user_model()
+
+        try:
+            # Only create if no superuser exists
+            if not User.objects.filter(is_superuser=True).exists():
+                User.objects.create_superuser(
+                    username='leonadmin',
+                    email='leonfreeman6@gmail.com',
+                    password='Project3Admin!'
+                )
+        except (OperationalError, ProgrammingError):
+            # This happens during initial migrations before auth tables exist – ignore.
+            pass
--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booking_app',
-
+    'booking_app.apps.BookingAppConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
