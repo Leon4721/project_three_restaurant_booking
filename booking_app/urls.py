1f9569f9c90cb7817bf7d7a58fd46ee85fae87ed
Leon4721 <leonfreeman6@gmail.com>
2025-12-11 16:29:25 +0000

    Security cleanup: removed create-admin route

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from booking_app.views import create_admin_once
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,17 +8,17 @@ urlpatterns = [
     path('manage/', views.manage_booking, name='manage_booking'),
 
     # Cancel booking
-    # 🔹 IMPORTANT: make sure your view is called cancel_booking in views.py
     path('cancel/', views.cancel_booking, name='cancel_booking'),
 
-    # Optional extra views if you have them
+    # Edit booking
     path('edit/<str:booking_code>/', views.edit_booking, name='edit_booking'),
+
+    # Booking detail
     path('detail/<str:booking_code>/', views.booking_detail, name='booking_detail'),
 
-    # Booking confirmation page
-    # If your booking_code is a UUIDField, uuid is fine here
-   path('confirmed/<str:code>/', views.booking_confirmation, name='booking_confirmation'),
-   path('staff-login/', views.staff_login, name='staff_login'),
-   path("create-admin/", create_admin_once),
+    # Booking confirmation
+    path('confirmed/<str:code>/', views.booking_confirmation, name='booking_confirmation'),
 
+    # Staff login
+    path('staff-login/', views.staff_login, name='staff_login'),
 ]
